68b287434307dc169e999cde8e77109b81172413
sharet9446 <spees97@naver.com>
2025-01-20 12:08:59 +0900

    init

--- part1/09.콜백함수/(lv.1)콜백-함수-실행(2).js
+++ part1/09.콜백함수/(lv.1)콜백-함수-실행(2).js
@@ -11,7 +11,7 @@
  * @returns {number} 최종 콜백 체인 결과
  */
 function chainCallbacks(initialValue, callback1, callback2) {
-  // TODO: 두 콜백을 차례로 호출한 결과값을 반환하세요.
+  return callback2(callback1(initialValue))
 }
 
 // export를 수정하지 마세요.
